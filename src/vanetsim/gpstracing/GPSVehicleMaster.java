3d3f45f3ffdee53b6e42f169756fb5e71c38a629
Felix Kiehn <9kiehn@informatik.uni-hamburg.de>
2014-11-23 13:01:45 +0100

    Trafficmodel: Fixed a logical error in VehicleMaster

--- src/vanetsim/gpstracing/GPSVehicleMaster.java
+++ src/vanetsim/gpstracing/GPSVehicleMaster.java
@@ -3,58 +3,60 @@ package vanetsim.gpstracing;
 import vanetsim.map.Map;
 import vanetsim.scenario.Vehicle;
 
-
 public class GPSVehicleMaster {
-	
+
 	private static GPSVehicleMaster instance_;
-	
+
 	private SortedVehicleQueue vehicles_;
 	private boolean simulationIsRunning_ = false;
 	private long minimumAtStart_;
-	
+
 	/**
 	 * 
 	 */
-	private GPSVehicleMaster(){
+	private GPSVehicleMaster() {
 		vehicles_ = new SortedVehicleQueue();
 	}
-	
+
 	/**
 	 * 
 	 * @return
 	 */
-	public static GPSVehicleMaster getInstance(){
-		
-		if(instance_ == null){
+	public static GPSVehicleMaster getInstance() {
+
+		if (instance_ == null) {
 			instance_ = new GPSVehicleMaster();
 		}
-		
+
 		return instance_;
 	}
-	
-	public void addVehicle(Vehicle vehicle, long startTime){
-		if (!simulationIsRunning_){
+
+	public void addVehicle(Vehicle vehicle, long startTime) {
+		if (!simulationIsRunning_) {
 			vehicles_.add(vehicle, startTime);
 		}
 	}
-	
+
 	/**
 	 * 
 	 */
-	public void startSim() {
+	public void startSim() throws NoVehicleFoundException {
 		simulationIsRunning_ = true;
 		minimumAtStart_ = vehicles_.peek().getStartTime();
+		if (!vehicles_.isEmpty()) {
+			Map.getInstance().addVehicle(vehicles_.remove().getVehicle());
+		}
 	}
-	
+
 	/**
 	 * 
 	 * @param simTime
 	 */
-	public void receiveSimulationTime(long simTime){
-		while(vehicles_.peek().getStartTime() < minimumAtStart_ + simTime && !vehicles_.isEmpty()){
+	public void receiveSimulationTime(long simTime) {
+		while (vehicles_.peek().getStartTime() < minimumAtStart_ + simTime
+				&& !vehicles_.isEmpty()) {
 			Map.getInstance().addVehicle(vehicles_.remove().getVehicle());
 		}
 	}
-	
-	
+
 }
