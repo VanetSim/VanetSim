4eaee1c5c7420ddb2df497e2d4e4f97ba17f26ef
Elmo <9randsch@informatik.uni-hamburg.de>
2015-01-16 23:33:26 +0100

    added Nakagami Propagation

--- src/vanetsim/scenario/positionverification/PropagationModel.java
+++ src/vanetsim/scenario/positionverification/PropagationModel.java
@@ -18,19 +18,28 @@ public class PropagationModel {
     public static final int PROPAGATION_MODEL_NAKAGAMI = 3;
 
     /**
-     * The globally used Propagationmodel. This Setting is available in order to provide a mechanism to get a global config for all Vehicles.
-     * <code>0</code> if no global Model is used.
+     * The globally used Propagationmodel to calculate a RSSi. This Setting is available in order to provide a mechanism to get a global config for
+     * all Vehicles.
      */
     private static int globalPropagationModel = 1;
 
+    /** The Propagation Model globally used to calculate a Distance from a given RSSI */
+    private static int globalReversePropagationModel = 1;
+
     /** Random Number generator used for Gauss Distribution */
     private Random rand_ = new Random(System.currentTimeMillis());
+
+    /** a Gamma Distribution used for Nakagami Fading */
+    private GammaDistribution gammaDist = null;
 
     /** The standard deviation of the Gauss Distribution, used for e.g. Shadowing Propagation */
     static double sigma_ = 4;
 
     /** The mean of the Gauss Distribution, for Shadowing this is 0. */
     static double mean_ = 0;
+
+    /** The Scale Factor for the Gamma Distribution. */
+    static double SHAPE = 1.5;
 
     /** a reference Distance measured in meters */
     static double referenceDistance_ = 1;
@@ -39,7 +48,7 @@ public class PropagationModel {
      * Sending and receiving parameters set to 1, because they are assumed to be equal for every vehicle. Parameters exist in case we want to change
      * these.
      */
-    static double sendingPower_, sendingGain_, receivingGain_ = 1;
+    static double sendingPower_ = 30, sendingGain_, receivingGain_ = 1;
 
     /** Passloss Factor for propagation modells. */
     static double passLossFactor_ = 2;
@@ -50,6 +59,8 @@ public class PropagationModel {
 
     /** received signal strength at <code> referenceDistance_ </code> **/
     static double Pr_0 = Double.NaN;
+
+    private double scale;
 
     /** Empty constructor in order to disable instancing. */
     private PropagationModel() {
@@ -76,12 +87,21 @@ public class PropagationModel {
     }
 
     /** returns the global used Propagationmodel **/
-    public static int getGlobalPropagationModel() {
+    public static int getGlobalDistanceToRSSPropagationModel() {
         return globalPropagationModel;
     }
 
-    public static void setGlobalPropagationModel(int globalPropagationModel) {
-        PropagationModel.globalPropagationModel = globalPropagationModel;
+    /** returns the global used Propagationmodel **/
+    public static int getGlobalRSSToDistancePropagationModel() {
+        return globalReversePropagationModel;
+    }
+
+    public static void setGlobalDistanceToRSSPropagationModel(int model) {
+        PropagationModel.globalPropagationModel = model;
+    }
+
+    public static void setGlobalRssToDistancePropagationModel(int model) {
+        PropagationModel.globalReversePropagationModel = model;
     }
 
     public static double getSigma() {
@@ -163,7 +183,7 @@ public class PropagationModel {
      * Propagation Models:<br>
      * PropagationModel.PROPAGATION_MODEL_FREE_SPACE<br>
      * PropagationModel.PROPAGATION_MODEL_SHADOWING<br>
-     * <br>
+     * PropagationModel.PROPAGATION_MODEL_NAKAGAMI <br>
      * 
      * result will be the RSSI in [dB], uses dB because values are way better to interpret
      * 
@@ -173,7 +193,6 @@ public class PropagationModel {
      * @param dy
      * @return the calculated RSSI value, or <code>NaN</code> if no RSSI could be calculated.
      */
-
     public double calculateRSSI(int propagationModel, long dx, long dy) {
         double d = Math.sqrt(dx * dx + dy * dy);// / 100; // convert [cm] to [m] to not mess up the formulas
         // this might help if GPS Traces are used together with RSSI Traces to keep Numbers at scale
@@ -182,9 +201,25 @@ public class PropagationModel {
 
     }
 
-    public double calculateRSSI(int propagationModel, double in) {
+    /**
+     * Calculates the RSSI Value that would be received using a Propagation Model at the distance d. <br>
+     * <br>
+     * Propagation Models:<br>
+     * PropagationModel.PROPAGATION_MODEL_FREE_SPACE<br>
+     * PropagationModel.PROPAGATION_MODEL_SHADOWING<br>
+     * PropagationModel.PROPAGATION_MODEL_NAKAGAMI <br>
+     * 
+     * result will be the RSSI in [dB], uses dB because values are way better to interpret
+     * 
+     * @param propagationModel
+     *            the Propagation Model to use
+     * @param d
+     *            the Distance
+     * @return RSSI Value
+     */
+    public double calculateRSSI(int propagationModel, double d) {
         double result = Double.NaN;
-        double distance = in / 100;
+        double distance = d / 100;
 
         switch (propagationModel) {
             case PROPAGATION_MODEL_FREE_SPACE:
@@ -201,20 +236,11 @@ public class PropagationModel {
 
                 break;
             case PROPAGATION_MODEL_NAKAGAMI:
-                // TODO: implement this!
-                // Gamma Distributed Rx
-                //  pseudo
-                // double powerW = std::pow (10, (txPowerDbm - 30) / 10);
-                // resultPowerW = m_gammaRandomVariable->GetValue (m, powerW / m);
-                // double resultPowerDbm = 10 * std::log10 (resultPowerW) + 30;
-
-                /*
-                 * The Gamma distribution density function has the form
-                 * 
-                 * x^(alpha-1) * exp(-x/beta) p(x; alpha, beta) = ---------------------------- beta^alpha * Gamma(alpha)
-                 * 
-                 * for x > 0.
-                 */
+                if (gammaDist == null) {
+                    scale = sendingPower_ / SHAPE;
+                    gammaDist = new GammaDistribution(SHAPE, scale);
+                }
+                result = (10 * Math.log10(gammaDist.sample())) * -1.0;
 
                 break;
             default:
@@ -225,7 +251,10 @@ public class PropagationModel {
     }
 
     /**
-     * calculates a distance to the rssi value.
+     * calculates a distance to a given RSS value. <br>
+     * Propagation Models:<br>
+     * PropagationModel.PROPAGATION_MODEL_FREE_SPACE<br>
+     * PropagationModel.PROPAGATION_MODEL_SHADOWING<br>
      * 
      * @param propagationModel
      *            the propagationmodel to be used
@@ -254,9 +283,6 @@ public class PropagationModel {
                 // calculate a distance from the rssi value
                 result = referenceDistance_ * Math.pow(10, (Pr_0 + X - rssi) / (10 * passLossFactor_));
 
-                break;
-            case PROPAGATION_MODEL_NAKAGAMI:
-                // TODO: implement this!
                 break;
             default:
                 result = Double.NaN;
